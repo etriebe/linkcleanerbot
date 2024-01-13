319e6e2ca1489ed515ff95eaef5b5a747281dced
Eric Triebe <eric.triebe@gmail.com>
2024-01-13 14:05:08 -0800

    Get pickem query working

--- commands/utility/pickem.js
+++ commands/utility/pickem.js
@@ -1,8 +1,10 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, codeBlock } = require('discord.js');
 const fs = require('node:fs');
-const { arbemPath } = require('../../config.json');
+const { arbemPath, arbemDirectory } = require('../../config.json');
 const { promisify } = require('util');
 const exec = promisify(require('child_process').exec);
+const execFile = promisify(require('child_process').execFile);
+const execFileSync = promisify(require('child_process').execFileSync);
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -93,31 +95,47 @@ module.exports = {
 			if (person)
 			{
 				args.push('--person');
-				args.push(person);
+				args.push(`"${person}"`);
 			}
 			if (dayofweek)
 			{
 				args.push('--dayofweek');
-				args.push(dayofweek);
+				args.push(`"${dayofweek}"`);
 			}
 			if (teams)
 			{
 				args.push('--teams');
-				args.push(teams);
+				args.push(`"${teams}"`);
 			}
+			// const result = await RunArbemExecFile(args);
+			await interaction.deferReply();
 			const result = await RunArbem(args);
+			await interaction.editReply(codeBlock(result));
 		}
 	},
 };
 
 async function RunArbem(args)
 {
-	let output = '';
 	const fullCommand = `${arbemPath} ${args.join(' ')}`;
-	const arbemOutput = await exec(fullCommand);
+	console.log(`Running: ${fullCommand}`);
+	const arbemOutput = await execFile(arbemPath, args, { cwd: arbemDirectory });
 	if (arbemOutput)
 	{
 		return arbemOutput.stdout.trim();
 	}
-	return 'Ran arbem';
+	return 'Ran arbem but no output';
+}
+
+async function RunArbemExecFile(args)
+{
+	const child = execFile(arbemPath, args, (error, stdout, stderr) =>
+	{
+		if (error)
+		{
+			console.error('stderr', stderr);
+			throw error;
+		}
+		console.log('stdout', stdout);
+	});
 }
